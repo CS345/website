b1c47def8718d7949fe6eca2f1d4e549536ac8d8
Oren Rasekh <orenr61@gmail.com>
2012-04-24 14:16:16 -0400

    updated references to registration servlet

--- src/cs345/servlets/Register.java
+++ src/cs345/servlets/Register.java
@@ -25,7 +25,7 @@ public class Register extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("got registration form!");
 	}
 
 }
